9c514020f235c300b2c11357830b2b67c90562ce
davidfernndez775 <davidfernndez775@yahoo.com>
2025-01-16 16:21:17 -0500

    aproach from devcommunity to verify email still in progress

--- backend/app/authentication/urls.py
+++ backend/app/authentication/urls.py
@@ -7,16 +7,16 @@ from django.urls import path, re_path
 app_name = 'authentication'
 
 urlpatterns = [
-    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+
     path("register/", CustomRegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path("user/", CustomUpdateView.as_view(), name="rest_user_details"),
     # Rutas de dj-rest-auth
-    path('verify-email/',
-         VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('account-confirm-email/',
-         VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
-            VerifyEmailView.as_view(), name='account_confirm_email'),
+    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    re_path(
+        r'^account-confirm-email/(?P<key>[-:\w]+)/$',
+        ConfirmEmailView.as_view(),
+        name='account_confirm_email',
+    ),
 ]
--- backend/app/authentication/views.py
+++ backend/app/authentication/views.py
@@ -1,7 +1,10 @@
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import UserDetailsView
 from authentication.serializers import CustomRegisterSerializer, CustomUserDetailsSerializer
+from django.urls import reverse
 from allauth.account.utils import send_email_confirmation
+from allauth.account.adapter import get_adapter
+from allauth.account.models import EmailConfirmationHMAC
 
 
 class CustomRegisterView(RegisterView):
@@ -12,6 +15,12 @@ class CustomRegisterView(RegisterView):
         user = serializer.save(request=self.request)
         # Enviar confirmación de correo electrónico
         send_email_confirmation(request=self.request, user=user)
+        # Update the reverse call to include the app_name for email confirmation
+        email_confirmation = EmailConfirmationHMAC(user)
+        activate_url = reverse('authentication:account_confirm_email', args=[
+                               email_confirmation.key])
+        get_adapter().send_confirmation_mail(
+            self.request, email_confirmation, signup=True)
         return user
 
 
