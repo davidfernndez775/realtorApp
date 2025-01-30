14ff61570dd20576a569b2c9920d7e7600af708d
David Fernandez <davidfernndez775@yahoo.com>
2025-01-30 13:56:49 -0500

    Update filters.py

--- backend/app/realstateproperties/filters.py
+++ backend/app/realstateproperties/filters.py
@@ -15,17 +15,42 @@ class RealEstatePropertyFilter(django_filters.FilterSet):
         field_name='for_rent_or_sale',
         choices=RealEstateProperty.PropertyStatus.choices
     )
-    price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
-    beds = django_filters.NumberFilter(field_name='beds', lookup_expr='gte')
-    full_baths = django_filters.NumberFilter(
+    # Rango de valores para price
+    price_min = django_filters.NumberFilter(
+        field_name='price', lookup_expr='gte')
+    price_max = django_filters.NumberFilter(
+        field_name='price', lookup_expr='lte')
+
+    # Rango de valores para beds
+    beds_min = django_filters.NumberFilter(
+        field_name='beds', lookup_expr='gte')
+    beds_max = django_filters.NumberFilter(
+        field_name='beds', lookup_expr='lte')
+
+    # Rango de valores para full_baths
+    full_baths_min = django_filters.NumberFilter(
         field_name='full_baths', lookup_expr='gte')
-    half_baths = django_filters.NumberFilter(
+    full_baths_max = django_filters.NumberFilter(
+        field_name='full_baths', lookup_expr='lte')
+
+    # Rango de valores para half_baths
+    half_baths_min = django_filters.NumberFilter(
         field_name='half_baths', lookup_expr='gte')
-    water_front = django_filters.BooleanFilter(field_name='water_front')
-    built = django_filters.NumberFilter(
-        field_name='built', lookup_expr='exact')
+    half_baths_max = django_filters.NumberFilter(
+        field_name='half_baths', lookup_expr='lte')
+
+    # Rango de valores para built (año de construcción)
+    built_min = django_filters.NumberFilter(
+        field_name='built', lookup_expr='gte')
+    built_max = django_filters.NumberFilter(
+        field_name='built', lookup_expr='lte')
 
     class Meta:
         model = RealEstateProperty
-        fields = ['property_type', 'price', 'county', 'beds',
-                  'full_baths', 'half_baths', 'water_front', 'built']
+        fields = [
+            'property_type', 'county', 'for_rent_or_sale',
+            'price_min', 'price_max', 'beds_min', 'beds_max',
+            'full_baths_min', 'full_baths_max', 'half_baths_min', 'half_baths_max',
+            'built_min', 'built_max'
+        ]
+
