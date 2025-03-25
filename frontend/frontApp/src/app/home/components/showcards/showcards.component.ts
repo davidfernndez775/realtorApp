b023c8a594d9f2f034f6628847cc5d59a51fea71
davidfernndez775 <davidfernndez775@yahoo.com>
2025-03-25 14:55:44 -0400

    animation in progress in business section

--- frontend/frontApp/src/app/home/components/showcards/showcards.component.ts
+++ frontend/frontApp/src/app/home/components/showcards/showcards.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-showcards',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './showcards.component.html',
   styleUrl: './showcards.component.css',
 })
