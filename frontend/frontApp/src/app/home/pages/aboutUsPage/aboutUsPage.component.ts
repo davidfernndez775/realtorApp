d7476aa29bbe32cd6c068603951f2a67758a15ca
davidfernndez775 <davidfernndez775@yahoo.com>
2025-03-26 10:02:30 -0400

    about us page done

--- frontend/frontApp/src/app/home/pages/aboutUsPage/aboutUsPage.component.ts
+++ frontend/frontApp/src/app/home/pages/aboutUsPage/aboutUsPage.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AboutUsSectionComponent } from '../../sections/aboutUsSection/aboutUsSection.component';
 
 @Component({
   selector: 'app-about-us-page',
   standalone: true,
-  imports: [],
+  imports: [AboutUsSectionComponent],
   templateUrl: './aboutUsPage.component.html',
   styleUrl: './aboutUsPage.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutUsPageComponent { }
+export class AboutUsPageComponent {}
