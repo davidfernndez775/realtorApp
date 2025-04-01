68d94ebb2c2910067bcf705a19e1bae38d6d0da4
davidfernndez775 <davidfernndez775@yahoo.com>
2025-04-01 12:10:19 -0400

    adding a modal for properties detail from list component

--- frontend/frontApp/src/app/properties/components/list/list.component.ts
+++ frontend/frontApp/src/app/properties/components/list/list.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  TemplateRef,
+  ViewEncapsulation,
+} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +13,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
+  // encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListComponent { }
+export class ListComponent {
+  private modalService = inject(NgbModal);
+
+  openXl(content: TemplateRef<any>) {
+    this.modalService.open(content, { size: 'xl' });
+  }
+}
