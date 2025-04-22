aef17ce0221060a8a208276fa96b78a9e5a8179e
davidfernndez775 <davidfernndez775@yahoo.com>
2025-04-22 13:37:37 -0400

    adding router links between pages

--- frontend/frontApp/src/app/auth/pages/loginPage/loginPage.component.ts
+++ frontend/frontApp/src/app/auth/pages/loginPage/loginPage.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './loginPage.component.html',
   styleUrl: './loginPage.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {}
